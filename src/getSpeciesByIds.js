da91aac077b6021c8e6d751fcf15cb2341a57454
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-03-16 17:00:10 -0300

    Nao finaliza 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const { species } = data;
 
+// console.log(species);
+
+function getSpeciesByIds(ids) {
+  const getId = species.filter((spe) => spe.id === ids);
+  return getId;
+}
+// const getId = species.map((spe) => `${spe.id} ${spe.location} ${spe.availability} ${spe.name}
+// ${spe.popularity}  ${spe.residents}`);
+// // console.log(getId);
+
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
