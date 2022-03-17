3989ffb968e67357f0485a9840d4461961f2a5ad
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-03-17 13:35:36 -0300

    Desafio 3 2/3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return employees.find((name) => name.firstName === employeeName || 
+  name.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
