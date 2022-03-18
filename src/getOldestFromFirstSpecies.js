7b4e6b38bbf54a5e8d12f39c99ac1a7b9fbf5c66
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-03-18 12:03:37 -0300

    Resolve Desafio 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,11 +1,12 @@
 const data = require('../data/zoo_data');
 
 const { employees } = data;
-const { species } = data
+const { species } = data;
 function getOldestFromFirstSpecies(id) {
   const firstAnimalId = employees.find((emp) => emp.id === id).responsibleFor[0];
   const resiAnimal = species.find((animal) => animal.id === firstAnimalId).residents;
-  return resiAnimal;
+  const olderThan = resiAnimal.sort((a, b) => b.age - a.age);
+  return [olderThan[0].name, olderThan[0].sex, olderThan[0].age];
 }
 console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getOldestFromFirstSpecies;
