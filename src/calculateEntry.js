0db4daaceb662d3814515e34dd6f422719b94d3f
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-03-23 00:20:39 -0300

    primeira function do desafio 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
 function countEntrants(entrants) {
-  //  seu código aqui
+  return entrants.reduce((acc, cur) => {
+    if (cur.age < 18) {
+      acc.child += 1;
+    } else if (cur.age < 50) {
+      acc.adult += 1;
+    } else acc.senior += 1;
+    return acc;
+  },
+  { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
