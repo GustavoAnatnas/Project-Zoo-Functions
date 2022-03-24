93bc55c24ae15295765f5470454f6e75def1eaed
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-03-23 22:59:51 -0300

    Conclui desafio 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+function listAnimals(a, specie) {
+  return specie.residents.reduce((acc, cur) => {
+    if (a.sex && cur.sex === a.sex) acc.push(cur.name);
+    if (a.sex === undefined) acc.push(cur.name);
+    return acc;
+  }, []);
+}
+
+function getAnimalMap(param = { includeNames: false, sorted: false, sex: false }) {
+  const locAnimal = species.reduce((objList, specie) => {
+    if (param.includeNames) {
+      const animalList = listAnimals(param, specie);
+      if (param.sorted) {
+        animalList.sort();
+      }
+      objList[specie.location].push({ [specie.name]: animalList });
+    } else {
+      objList[specie.location].push(specie.name);
+    }
+    return objList;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+  return locAnimal;
 }
 
 module.exports = getAnimalMap;
